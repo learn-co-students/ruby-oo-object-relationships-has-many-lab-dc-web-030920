d2a1de16ff4f8fe929d4396a6dd450e9ddba0f48
kaidornel <kai_dornel@hotmail.com>
2020-03-11 23:27:37 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def posts
+        Post.all.select do |post_instance| 
+            @@all << post_instance
+        end
+        # @@all << stuff
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+
+    def add_post_by_title(post_title)
+        post = Post.new(post_title)
+        post.author = self
+    end
+
+    def self.post_count
+        Post.all.count
+    end
+end
